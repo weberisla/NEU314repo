dd0cf6c9f5dcb5ee791c6f39ef238550f55b029f
Isla Weber <weberisla@gmail.comn>
2019-10-04 19:43:28 -0400

    EX3 Final

--- NEU314_Exc3.jl
+++ NEU314_Exc3.jl
@@ -18,15 +18,40 @@ cd("/Users/islaweber/Documents/NEU314")
 imread("el-capitan.png")
 image = imread("el-capitan.png")
 imshow(image)
+size(image)
 
 ## b. c. d.
-function color_channels(image)
+
+"""
+color_channels(filename)
+
+Extracts red, blue, and green channels of an image file and returns them
+as a vector containing the three channel matrices.
+
+Args:
+   filename (image file): The name of the image file to read.
+
+Returns:
+   image_channels (vector): a vector containing the image data for the
+   red (R), green (G), and blue (B) channels.
+
+   image_channels has the shape (R, G, B), where R, G, and B are each
+   matrices with the same number of columns and rows as the pixels in
+   the image file.
+"""
+function color_channels(filename)
+    image = imread(filename)
+    # break image down into red, green, and blue channels:
     R = image[:,:,1]
     G = image[:,:,2]
     B = image[:,:,3]
+    # compile channels into one variable as a vector:
+    image_channels = (R, G, B)
     println("Reset with git reset --mixed")
-    return R, G, B
+    return image_channels
 end
+
+color_channels("el-capitan.png")
 
 ## e.
 image2 = zeros(360, 640, 3)
@@ -35,25 +60,42 @@ image2[:,:,2]=image[:,:,3]
 image2[:,:,3]=image[:,:,1]
 
 subplot(1,2,1)
+imshow(image)
+title("El Capitan")
+axis("off")
+
+subplot(1,2,2)
 imshow(image2)
-title("Normal")
+title("El Capitan - Color Shift")
 axis("off")
 
 ## f.
 
 # B.
+"""
+shift_red(image, pixels)
 
+Extracts the red channel of an image file and performs a circular rotation,
+moving the red channel up the given number of rows.
+
+Args:
+   image (image file): The name of the image file to read.
+   pixels (integer): the number of rows the channel will be shifted up.
+
+Returns:
+    This function returns a matrix containing the rotated red channel.
+"""
 function shift_red(image, pixels)
-    R = image[:,:,1]
+    R = imread(image)[:,:,1] # create a variable of the red channel matrix
     circshift(R, pixels)
 end
 
 subplot(1,2,1)
-imshow(image[:,:,1])
-title("Normal")
+imshow(imread("el-capitan.png")[:,:,1])
+title("Red Channel")
 axis("off")
 
 subplot(1,2,2)
-imshow(shift_red(image, 180))
-title("Shift")
+imshow(shift_red("el-capitan.png", 180))
+title("Rotated Red Channel")
 axis("off")
