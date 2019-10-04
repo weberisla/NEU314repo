c7ad393ee547b786bdae5aba5782af16dc49f5f7
Isla Weber <weberisla@gmail.comn>
2019-10-04 14:03:19 -0400

    EX3 A e.

--- NEU314_Exc3.jl
+++ NEU314_Exc3.jl
@@ -1,7 +1,11 @@
 # NEU 314 Excercises 3
 ## Isla Weber
 
-
+Pkg.add("PyCall")
+Pkg.build("PyCall")
+using PyCall
+Pkg.add("PyPlot")
+Pkg.build("PyPlot")
 using PyPlot
 using JLD
 using Images
@@ -15,20 +19,29 @@ imread("el-capitan.png")
 image = imread("el-capitan.png")
 imshow(image)
 
-## b.
+## b. c. d.
 function color_channels(image)
     R = image[:,:,1]
     G = image[:,:,2]
     B = image[:,:,3]
+    println("Reset with git reset --mixed")
     return R, G, B
 end
 
-## c.
-
-## d.
-
 ## e.
+image2 = zeros(360, 640, 3)
+image2[:,:,1]=image[:,:,2]
+image2[:,:,2]=image[:,:,3]
+image2[:,:,3]=image[:,:,1]
+
+subplot(1,2,1)
+imshow(image2)
+title("Normal")
+axis("off")
 
 ## f.
 
 # B.
+function shift_(image)
+    imshow(circshift(image, pixels))
+end
